143ea6a12a05d9a990b71352f1873e67539a1cd7
Daniel Cordova <javierdanielcordova@gmail.com>
2023-04-20 12:16:42 +0200

    ejercicio Listo

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 //En este array de strings, tenemos todos los colores
 const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple']
 
+/*Al hacer clic en el botón "Click Me!" el color del fondo de body siempre cambia al mismo color, por ejemplo, amarillo*/
 
+document.querySelector('button').addEventListener('click', function () {
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+    document.querySelector('body').style.backgroundColor = randomColor;
+});
